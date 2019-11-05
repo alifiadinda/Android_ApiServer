c919ea27333922c01472c816507d8cdb5fc6842b
alifiadinda <37792896+alifiadinda@users.noreply.github.com>
2019-11-06 04:08:55 +0700

    Client Android

---
+++ Retrofit/app/src/main/java/ti4i/alifia/com/retrofit/Api/ServiceApi.java
@@ -0,0 +1,18 @@
+package ti4i.alifia.com.retrofit.Api;
+
+import java.util.HashMap;
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface ServiceApi {
+    String BASE_URL = "http://192.168.43.171/api-image/";
+
+    @Multipart
+    @POST("index.php")
+    Call<HashMap<String, String>> convertImage(@Part("image\"; filename=\"myfile.png\" ") RequestBody file,
+                                               @Part("text") RequestBody name);
+}
